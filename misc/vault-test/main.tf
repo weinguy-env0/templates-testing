7345c2ed31ceebe1bea4cd752cb2ea98a3bcf3a2
Guy Weinberg <guy.weinberg@env0.com>
2023-11-14 12:19:42 +0200

    changed role

--- misc/vault-test/main.tf
+++ misc/vault-test/main.tf
@@ -1,7 +1,7 @@
 provider "vault" {
   address = "http://ab2fe5d8c6c3642cf893184fc93d6be7-1541439417.us-east-1.elb.amazonaws.com:8200"
   auth_login_jwt {
-    role = "test-role"
+    role = "guy-jwt/test-role"
   }
 }
 
