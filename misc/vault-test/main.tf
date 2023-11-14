c4f44ad70ffcd06411f8b7d5af3c4247e13c0c41
Guy Weinberg <guy.weinberg@env0.com>
2023-11-14 18:19:48 +0200

    replaced code again

--- misc/vault-test/main.tf
+++ misc/vault-test/main.tf
@@ -1,19 +1,2 @@
-provider "vault" {
-  address = "http://ab2fe5d8c6c3642cf893184fc93d6be7-1541439417.us-east-1.elb.amazonaws.com:8200"
-  auth_login_jwt {
-    role = "guy-jwt/test-role"
-  }
-}
-
 resource "null_resource" "null" {
 }
-
-resource "vault_generic_secret" "example_secret" {
-  path = "secret/example"
-
-  data_json = jsonencode({
-    username = "your_username",
-    password = "your_password",
-  })
-}
-
